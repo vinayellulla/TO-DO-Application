42751d0d55b9f93b09e485110d106ba7143cc971
vinayellulla <135804025+vinayellulla@users.noreply.github.com>
2024-09-26 20:27:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import Todo from "./Components/Todo";
+
+function App() {
+  return (
+    <>
+      <div className=" bg-stone-900 grid pd-40 py-4 min-h-screen">
+        <Todo />
+      </div>
+    </>
+  );
+}
+
+export default App;
